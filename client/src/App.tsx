1b7e82fe68bc6adf77df425b841d9a9463bd4f91
biswass101 <biswassnaeemcse@gmail.com>
2025-12-07 12:11:15 +0600

    feat(auth): initiate implementing auth and protected routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,40 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import CallPage from "./pages/CallPage";
+import OnboardingPage from "./pages/OnboardingPage";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios";
 
 function App() {
-  return (
-    <div className="h-screen text-5xl" data-theme="night">
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/signup" element={<SignupPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/notifications" element={<Notifications/>}/>
 
+  const {data: authData, isLoading} = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get('/auth/me')
+      return res.data;
+    }
+  });
+
+  
+
+  console.log(data);
+  console.log(isLoading);
+
+  return (
+    <div className="h-screen" data-theme="night">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
+      <Toaster />
     </div>
   );
 }
--- client/src/pages/NotificationsPage.tsx
+++ client/src/pages/NotificationsPage.tsx
@@ -1,7 +1,7 @@
 
 const NotificationsPage = () => {
   return (
-    <div>NotificationsPage </div>
+    <div>NotificationsPage</div>
   )
 }
 
